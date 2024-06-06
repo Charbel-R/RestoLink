fef2191189705032626c57ada353f3869b927c65
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-06 13:19:47 +0200

    created mock data !

--- server/database.js
+++ server/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 console.log(process.env.MONGO_URI)
-// const URI = 'mongodb://localhost:27017/restolink';
+const URI = 'mongodb://localhost:27017/restolink';
 
-const database = mongoose.connect(process.env.MONGO_URI);
+const database = mongoose.connect(process.env.MONGO_URI || URI);
 
 
 
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 // TODO add .env file to gitignore 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 const database = require('./database');
 const app = express();
 
