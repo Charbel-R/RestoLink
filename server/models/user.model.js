6fcfb7e89faec0034f06ce15d8fb734d4e48c0a4
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-07 11:31:20 +0200

    added profile pic to user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  profilePicture: {
+    type: String,
+    default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F4733409%2Fbasic_profile_ui_icon&psig=AOvVaw3fHZ7FHSLRcDxVS4OXPmxY&ust=1717793090617000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiHqqHsx4YDFQAAAAAdAAAAABAE'
+  }
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
