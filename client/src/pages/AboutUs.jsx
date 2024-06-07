2170e77d207083e8e91a0e5efd8fef9296b288ef
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-07 14:14:33 +0200

    updated the aboutUs page

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -2,6 +2,29 @@
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About US </h1>
+      <h3 className="text-xl font-bold">RestoLink</h3>
+      <p className="mb-4 text-slate-700">
+        Struggling to find the perfect ingredients or equipment for your restaurant? 
+        Need to fill open positions with qualified staff? 
+        RestoLink is your one-stop shop for all your restaurant needs! 
+        This app connects restaurant owners and suppliers, 
+        making it easier than ever to find the products you need and the talent to run your business.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Restaurant Owners: Browse a wide variety of restaurant supplies from trusted vendors, 
+        compare prices, and contact suppliers directly. 
+        Post open positions and connect with qualified candidates seeking 
+        new opportunities in the culinary industry.
+      </p>  
+      <p className="mb-4 text-slate-700">
+        Restaurant Suppliers: Showcase your products, reach a wider audience of 
+        restaurant owners. 
+      </p >
+      <p className="mb-4 text-slate-700">
+          RestoLink offers a user-friendly platform with a focus on efficient connections. 
+      </p>
+    </div>
   )
 }
