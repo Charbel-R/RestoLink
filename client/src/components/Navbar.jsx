dfedc93e9dedb313e89b9b528670003d22b2a39f
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-07 20:14:47 +0200

    Update header component and make profile private

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+
 
 export default function Navbar() {
+  const { currentUser } = useSelector(state => state.user);
+
   return (
     <div className='bg-blue-800 text-white'>
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,12 +18,19 @@ export default function Navbar() {
           <Link to={'/about'}>
             <li>About</li>
           </Link>
-          <Link to={'/sign-in'}>
-            <li>Sign In</li> 
+          <Link to={'/profile'}>
+            {currentUser ? (
+              <img 
+              src={currentUser.userInfo.profilePicture} 
+              alt='profile'
+              className="h-7 w-7 rounded-full object-cover"/>
+            ) : (
+              <li>Sign In</li> 
+            )}
           </Link>
-          <Link to={'/sign-up'}>
-            <li>Register</li> 
-          </Link>
+          {/* <Link to={'/sign-up'}>
+            {currentUser ? '' :  <li>Register</li> }
+          </Link> */}
         </ul>
       </div>
     </div>
